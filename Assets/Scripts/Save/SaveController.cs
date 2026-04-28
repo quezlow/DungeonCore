d29ab44b78cda10c3227ab38ad1d59431b8567b8
quezlow <brpatte@gmail.com>
2026-04-28 12:40:26 -0500

    Added saving/loading

---
+++ Assets/Scripts/Save/SaveController.cs
@@ -0,0 +1,43 @@
+using System.IO;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation;
+
+    void Start()
+    {
+        //define save location
+        saveLocation = Path.Combine(Application.persistentDataPath, "SaveData.json");
+
+        LoadGame();
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindAnyObjectByType<CinemachineConfiner2D>().BoundingShape2D.gameObject.name
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            FindAnyObjectByType<CinemachineConfiner2D>().BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        }
+        else
+        {
+            SaveGame();
+        }
+    }
+}
