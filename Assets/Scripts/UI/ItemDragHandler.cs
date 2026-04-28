b9188a6b3cfcdbf32bbc8b403f77aae074f8487e
quezlow <brpatte@gmail.com>
2026-04-28 13:44:17 -0500

    More Inventory

--- Assets/Scripts/UI/ItemDragHandler.cs
+++ Assets/Scripts/UI/ItemDragHandler.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemDragHandler : MonoBehaviour
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Transform originalParent;
+    CanvasGroup canvasGroup;
+
+
     void Start()
     {
-        
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent; //save original parent
+        transform.SetParent(transform.root); //Above other canvas
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f; //semi transparent during drag
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true; //enables raycasts
+        canvasGroup.alpha = 1f; //no longer transparent
+
+        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>(); //slot where ite is dropped
+        if(dropSlot == null)
+        {
+            GameObject dropItem = eventData.pointerEnter;
+            if( dropItem != null )
+            {
+                dropSlot = dropItem.GetComponentInParent<Slot>();
+            }
+        }
+
+        Slot originalSlot = originalParent.GetComponent<Slot>();
+
+        if(dropSlot != null)
+        {
+            if(dropSlot.currentItem != null)
+            {
+                //slot has an item - swap items
+                dropSlot.currentItem.transform.SetParent(originalSlot.transform);
+                originalSlot.currentItem = dropSlot.currentItem;
+                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            }
+            else
+            {
+                originalSlot.currentItem = null;
+            }
+
+            //move item into drop slot
+            transform.SetParent(dropSlot.transform);
+            dropSlot.currentItem = gameObject;
+        }
+        else
+        {
+            //no slot under drop point
+            transform.SetParent(originalParent);
+        }
+
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
     }
 }
