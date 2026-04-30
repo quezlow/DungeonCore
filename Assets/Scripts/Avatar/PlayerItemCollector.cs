3c8e15614635d7bee03dcd66b476db2961992e2c
quezlow <brpatte@gmail.com>
2026-04-30 13:39:40 -0500

    Item popup ui

--- Assets/Scripts/Avatar/PlayerItemCollector.cs
+++ Assets/Scripts/Avatar/PlayerItemCollector.cs
@@ -21,6 +21,7 @@ public class PlayerItemCollector : MonoBehaviour
 
                 if (itemAdded)
                 {
+                    item.Pickup();
                     Destroy(collision.gameObject);
                 }
             }
