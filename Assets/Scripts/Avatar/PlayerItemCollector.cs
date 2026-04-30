2ce1d100f78126319ad8de48617166b27cfcd69d
quezlow <brpatte@gmail.com>
2026-04-30 12:59:48 -0500

    Item pickup

---
+++ Assets/Scripts/Avatar/PlayerItemCollector.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerItemCollector : MonoBehaviour
+{
+    private InventoryController inventoryController;
+
+    void Start()
+    {
+        inventoryController = FindAnyObjectByType<InventoryController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            Item item = collision.GetComponent<Item>();
+            if (item != null)
+            {
+                //Add item to inventory
+                bool itemAdded = inventoryController.AddItem(collision.gameObject);
+
+                if (itemAdded)
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/InventoryController.cs
+++ Assets/Scripts/UI/InventoryController.cs
@@ -17,6 +17,26 @@ public class InventoryController : MonoBehaviour
         itemDictionary = FindAnyObjectByType<ItemDictionary>();
     }
 
+    public bool AddItem(GameObject itemPrefab)
+    {
+        //Look for empty slot
+        foreach(Transform slotTransform in inventoryPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if(slot != null && slot.currentItem == null)
+            {
+                GameObject newItem = Instantiate(itemPrefab, slotTransform);
+                newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = newItem;
+                return true;
+            }
+        }
+
+        Debug.Log("Inventory is full!");
+        return false;
+    }
+
+
     public List<InventorySaveData> GetInventoryItems()
     {
         List<InventorySaveData> invData = new List<InventorySaveData>();
