244dc35b86a641963d80f6a858b31c8c29236559
quezlow <brpatte@gmail.com>
2026-05-13 14:49:02 -0500

    Started quest system, added item sprites

---
+++ Assets/Scripts/Progression/Quest.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quests/Quest")]
+public class Quest : ScriptableObject
+{
+    public string questID;
+    public string questName;
+    public string Description;
+    public List<QuestObjective> objectives;
+
+    private void OnValidate()
+    {
+        if (string.IsNullOrEmpty(questID))
+        {
+            questID = questName + Guid.NewGuid().ToString();
+        }
+    }
+}
+
+[System.Serializable]
+public class QuestObjective
+{
+    public string objectiveID; //match with item ID that you need to collect, enemy to kill, etc.
+    public string description;
+    public ObjectiveType type;
+    public int requiredAmount;
+    public int currentAmount;
+
+    public bool IsCompleted => currentAmount >= requiredAmount;
+}
+
+public enum ObjectiveType { CollectItem, DefeatEnemy, ReachLocation, TalkNPC, Custom }
+
+[System.Serializable]
+public class QuestProgress
+{
+    public Quest quest;
+    public List<QuestObjective> objectives;
+
+    public QuestProgress(Quest quest)
+    {
+        this.quest = quest;
+        objectives = new List<QuestObjective>();
+
+        foreach (var obj in quest.objectives)
+        {
+            objectives.Add(new QuestObjective
+            {
+                objectiveID = obj.objectiveID,
+                description = obj.description,
+                type = obj.type,
+                requiredAmount = obj.requiredAmount,
+                currentAmount = 0
+            });
+        }
+    }
+
+    public bool IsCompleted => objectives.TrueForAll(o => o.IsCompleted);
+
+    public string QuestID => quest.questID;
+}
---
+++ Assets/Scripts/UI/QuestUI.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestUI : MonoBehaviour
+{
+    public Transform questListContent;
+    public GameObject questEntryPrefab;
+    public GameObject objectiveTextPrefab;
+
+    public Quest testQuest;
+    public int testQuestAmount;
+    public List<QuestProgress> testQuests = new();
+
+    void Start()
+    {
+        for(int i = 0; i < testQuestAmount; i++)
+        {
+            testQuests.Add(new QuestProgress(testQuest));
+        }
+
+        UpdateQuestUI();
+    }
+
+    public void UpdateQuestUI()
+    {
+        foreach(Transform child in questListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach(var quest in testQuests)
+        {
+            GameObject entry = Instantiate(questEntryPrefab, questListContent);
+            TMP_Text questNameText = entry.transform.Find("QuestNameText").GetComponent<TMP_Text>();
+            Transform objectiveList = entry.transform.Find("ObjectiveList");
+
+            questNameText.text = quest.quest.name;
+
+            foreach(var objective in quest.objectives)
+            {
+                GameObject objTextGO = Instantiate(objectiveTextPrefab, objectiveList);
+                TMP_Text objText = objTextGO.GetComponent<TMP_Text>();
+                objText.text = $"{objective.description} ({objective.currentAmount}/{objective.requiredAmount})";
+            }
+        }
+    }
+}
